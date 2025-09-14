10dee85159689a34f3dfdf321a2f5eb26bb38dcb
banbxio <a31521424@163.com>
2025-09-14 23:50:28 +0800

    ⬆️ deps(frontend): bump vite 6.3.4 → 7.1.5 and update lockfile

--- frontend/vite.plugins.config.ts
+++ frontend/vite.plugins.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
                 inlineDynamicImports: true,
             },
         },
-        minify: false,
+        // minify: true,
         // sourcemap: true,
     }
 })
